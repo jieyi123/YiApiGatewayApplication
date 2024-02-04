b03420a45b72b3fcd7625ca9d8f1a2d8c4c867dd
jieyi123 <pjieyi@foxmail.com>
2024-02-04 14:57:55 +0800

    Initial commit

---
+++ src/main/java/com/pjieyi/yiapigatewayapplication/YiApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pjieyi.yiapigatewayapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjieyi/yiapigatewayapplication/YiApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjieyi.yiapigatewayapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
